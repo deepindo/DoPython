dcfdf1f5b9b5440e089869c8f14e0526dce78395
deepin <xiaobing.du@bizatmobile.com>
2022-01-12 16:58:02 +0800

    done institution_code custom format increment and other features

--- institution/admin.py
+++ institution/admin.py
@@ -27,60 +27,40 @@ class InstitutionAdmin(admin.ModelAdmin):
     # 要显示的字段
     # list_display = ('institution_code', 'name', 'alias', 'province', 'city', 'area', 'address', 'institution_type',
     #                 'institution_property', 'institution_character', 'post_number', 'phone', 'approve_status',
-    #                 'submit_date',)
-    list_display = ('institution_code', 'name', 'alias', 'detail_address', 'institution_type',
-                    'institution_property', 'institution_character', 'post_number', 'phone', 'approve_status',
-                    'submit_date', 'detail_info',)
+    #                 'create_date',)
+    list_display = ('serial_number', 'institution_code', 'name', 'alias', 'full_address', 'institution_type',
+                    'institution_property', 'institution_character', 'phone', 'post_number', 'approve_status',
+                    'create_date', 'operate_detail',)
 
     # 可以点击跳转的字段, 可以设置为None，一个不要
-    list_display_links = None
-    # list_display_links = ('name',)
+    # list_display_links = None
+    list_display_links = ('name',)
 
     # 对于添加和修改，只读字段的不同处理
     def get_readonly_fields(self, request, obj=None):
         if obj:
             return ['institution_code', "institution_type", "institution_property", 'institution_character', 'approve_status',
-                    'submit_date', ]
+                    'create_date', ]
         else:
             return ['institution_code', ]
 
     # 只读字段
     # readonly_fields = ('institution_code',)
 
-
-
-    # 增加Institution页面-展示(展示在同一行的放在一个元祖中, 对于一个元祖中超过两个，那么这多个会相对上下距离较近)
-    # fields = (
-    # ('name', 'alias'), ('institution_code', 'institution_type'), ('institution_property', 'institution_character'),
-    # ('province', 'city'), ('area', 'address'), ('post_number', 'phone'), 'approve_status', 'submit_date',)
-
-    fieldsets = (
-        ('基本信息', {
-            'fields': ('institution_code', 'name', 'alias', 'institution_type', 'institution_property', 'institution_character',)
-        }),
-        ('其他信息', {
-            'fields': (('province', 'city'), ('area', 'address'), ('post_number', 'phone'), )
-        }),
-        ('操作信息', {
-            'fields': ('approve_status', 'submit_date',)
-        }),
-    )
-
     # 模糊搜索功能
     search_fields = ('code', 'name', 'alias', 'province', 'city', 'area', 'address',)
 
     # 筛选功能 admin.RelatedFieldListFilter, admin.EmptyFieldListFilter, admin.RelatedOnlyFieldListFilter
-    list_filter = ('institution_type', 'institution_property', 'institution_character', 'approve_status',)
+    list_filter = ('institution_type', 'institution_property', 'institution_character', 'create_date', 'approve_status',)
 
     # 不设置的时候，是降序，最近加的显示在最上面
-    ordering = ('submit_date',)
+    ordering = ('institution_code',)
 
     # 每页显示条目数
     list_per_page = 10
 
     # list_editable = ('phone',)
-
-    # date_hierarchy = 'submit_date'
+    # date_hierarchy = 'create_date'
 
     actions = ['approve_institution', ]
     # ["export_as_csv", ]
@@ -92,10 +72,24 @@ class InstitutionAdmin(admin.ModelAdmin):
 
     # actions_selection_counter =
 
+    # 增加Institution页面-展示(展示在同一行的放在一个元祖中, 对于一个元祖中超过两个，那么这多个会相对上下距离较近)
+    # fields = (
+    # ('name', 'alias'), ('institution_code', 'institution_type'), ('institution_property', 'institution_character'),
+    # ('province', 'city'), ('area', 'address'), ('post_number', 'phone'), 'approve_status', 'create_date',)
+
+    fieldsets = (
+        ('基本信息', {
+            'fields': ('institution_code', ('name', 'alias'), ('province', 'city'), ('area', 'address'), ('phone', 'post_number'), )
+        }),
+        ('其他信息', {
+            'fields': ('institution_type', 'institution_property', 'institution_character', 'approve_status', 'create_date',)
+        }),
+    )
+
+    """自定义操作：批量审批"""
     def approve_institution(self, request, queryset):
         queryset.update(approve_status=2)
     approve_institution.short_description = "批量审批"
-
 
     # def export_excel(self, request, queryset):
     #     print("test export")
--- institution/models.py
+++ institution/models.py
@@ -40,38 +40,53 @@ class Institution(models.Model):
         (3, '审批拒绝'),
     )
 
-    # default=uuid.uuid4
+    """模型字段"""
     institution_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
-    institution_code = models.CharField(max_length=20, default='HA'+'000001', verbose_name='机构编码',)
-    # serial_number = models.IntegerField(default=1, verbose_name='序号')
-    code = models.CharField(max_length=30, blank=True, verbose_name='编码')
-    name = models.CharField(max_length=10, verbose_name='机构名称')
-    alias = models.CharField(max_length=30, blank=True, null=True, verbose_name='机构别名')
+    serial_number = models.IntegerField(default=1, blank=False, null=False, verbose_name='序号')
+    institution_code = models.CharField(max_length=20, verbose_name='机构编码',)
+    name = models.CharField(max_length=50, verbose_name='机构名称')
+    alias = models.CharField(max_length=50, blank=True, null=True, verbose_name='机构别名')
     province = models.CharField(max_length=20, verbose_name='省份')
     city = models.CharField(max_length=20, verbose_name='城市')
     area = models.CharField(max_length=20, blank=True, null=True, verbose_name='区县')
     address = models.CharField(max_length=100, blank=True, null=True, verbose_name='详细地址')
-    institution_type = models.CharField(max_length=100, choices=InstitutionType, default='其他', verbose_name='机构类别')
+    institution_type = models.CharField(max_length=100, choices=InstitutionType, verbose_name='机构类别')
     institution_property = models.IntegerField(choices=InstitutionProperty, verbose_name='机构性质')
-    institution_character = models.IntegerField(choices=InstitutionCharacter, verbose_name='机构属性')
+    institution_character = models.IntegerField(choices=InstitutionCharacter, default=1, verbose_name='机构属性')
     post_number = models.CharField(max_length=20, blank=True, null=True, verbose_name='邮政编码')
     phone = models.CharField(max_length=20, blank=True, null=True, verbose_name='机构电话')
     approve_status = models.IntegerField(choices=ApproveType, default=1, verbose_name='审批状态')
-    submit_date = models.DateTimeField(default=timezone.now, verbose_name='提交时间')
+    create_date = models.DateTimeField(default=timezone.now, verbose_name='创建时间')
 
-    # def institution_count(self):
-    #     return self.institution_id_set.count()
-
-    def detail_address(self):
+    # 自定义显示字段：完整地址
+    def full_address(self):
         return self.province + '-' + self.city + '' + self.area + '' + self.address
-    detail_address.short_description = '详细地址'
+    full_address.short_description = '机构地址'
 
-    def detail_info(self):
+    # 自定义添加操作动作
+    def operate_detail(self):
         return mark_safe('<a href="www.baidu.com" target="blank">详情</a>')
-    detail_info.short_description = "操作"
+    operate_detail.short_description = "操作"
 
     def __str__(self):
         return self.name
+
+    # 重写save方法
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+
+        # 若是self.institution_code为None，才是insert数据，而不是update数据
+        if self.institution_code is None:
+            print(self.institution_code)
+            # 获取数据库当前数据条数, 新的将在当前基础上加1
+            new_count = Institution.objects.count() + 1
+
+            # 对于机构编码institution_code，也可以用另外一种方法实现：self.institution_code = 'HA' + count.zfill(6)
+            self.institution_code = 'HA' + '%06d' % new_count
+            # 序号
+            self.serial_number = new_count
+
+        super().save()
 
     class Meta:
         verbose_name = verbose_name_plural = '机构信息'
