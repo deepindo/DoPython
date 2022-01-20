4ea8b3c002c7b69bbc9fb297332be2939d832445
deepin <xiaobing.du@bizatmobile.com>
2022-01-20 18:00:25 +0800

    try to do the api feature

--- institution/views.py
+++ institution/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from institution.models import Institution
 from django.http import JsonResponse
+from fastapi import FastAPI
+from django.views.decorators.http import require_http_methods
+from django.core import serializers
+import json
+# import requests
+
+# app = FastAPI()
+
+
+# @require_http_methods(['GET'])
+@require_http_methods(['POST'])
+def institution_add(request):
+    response = {}
+    try:
+        # institution = Institution(institution_name=request.GET.get('institution_name'))
+        institution = Institution(institution_name=request.POST.get('institution_name'))
+        institution.save()
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+
+    return JsonResponse(response)
+
+
+@require_http_methods(['GET'])
+def institution_list(request):
+    response = {}
+    try:
+        institutions = Institution.objects.filter()
+        response['data'] = json.loads(serializers.serialize('json', institutions))
+        response['msg'] = 'success'
+        response['error_num'] = 0
+    except Exception as e:
+        response['msg'] = str(e)
+        response['error_num'] = 1
+
+    return JsonResponse(response)
 
 
 def institution_list(request):
